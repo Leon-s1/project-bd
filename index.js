ef89ecd1b1bf1799a69f94315179152e190e87f5
leon-s1 <chuprov.sa@gmail.com>
2021-03-30 16:37:36 +0700

    add

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express') //Пакет поднимает сервер и обрабатывает запросы
+const mongoose = require('mongoose') //Пакет для работы с БД mongodb
+const exphbs = require('express-handlebars') //Движок представлений (шаблонов)
+const userRoutes = require('./routes/users')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+const hbs = exphbs.create({
+  //настройка конфигурации для шаблонизатора
+  defaultLayout: 'main',
+  extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs') //используем по умолчанию handlebars
+app.set('views', 'views')
+
+app.use(userRoutes)
 
 async function start() {
   try {
-    // Подключается база
-    await mongoose.connect('', {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    })
+    // Подключаем базу
+    await mongoose.connect(
+      'mongodb+srv://sergey:1q2w3e@cluster0.suv9b.mongodb.net/usersn',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+      }
+    )
     // Запускаем сервер
     app.listen(PORT, () => {
       console.log('Server has been started...')
