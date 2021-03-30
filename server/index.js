ff1fc44bd4a4106db4e293b8818994f4bd3fb890
Leon-s1 <chuprov.sa@gmail.com>
2021-03-30 21:05:58 +0700

    change folder server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
-console.log('Server has been started...')
+const express = require('express') //фреймворк для разработке веб на node.js
+const mongoose = require('mongoose') //библиотека для работы с БД mongodb
+const config = require('config')
+const PORT = config.get('serverPort')
+
+const app = express() //создаем сервер
+
+const start = async () => {
+  try {
+    // Подключаем базу
+    await mongoose.connect(
+      config.get('dbUrl', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+    )
+    //Запускаем сервер
+    app.listen(PORT, () => {
+      console.log('Server has been started on PORT', PORT)
+    })
+  } catch (e) {}
+}
+
+start()
